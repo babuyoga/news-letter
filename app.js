4595828f5374eb67d3ac3da02405eadf99a79bf7
babuyoga <56405793+babuyoga@users.noreply.github.com>
2021-05-26 22:59:42 +0530

    Complete first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const request = require("request");
 const https = require("https");
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static("public"));
@@ -52,7 +52,7 @@ console.log(firstName,lastName,email);
 
 });
 
-app.listen(3000,function() {
+app.listen(PORT,function() {
   console.log("Server is running on port 3000");
 });
 
